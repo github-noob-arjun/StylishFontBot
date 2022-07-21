31e6d3c5efbeb8f5f7310d24c5da6090b3a4901d
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-21 11:07:09 +0530

    Update Fontbot.py

--- Font/Fontbot.py
+++ Font/Fontbot.py
@@ -1,18 +1,10 @@
 import pyrogram
-
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 import os
-
 from config import Config
 from pyrogram import Client 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 FontBot = Client(
-      "ShowJson",
+      "FontBot",
       bot_token=Config.BOT_TOKEN,
       api_id=Config.API_ID,
       api_hash=Config.API_HASH
