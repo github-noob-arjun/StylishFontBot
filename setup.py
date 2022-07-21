7d25ef2b34b6aa5f83dd021915c2109dcfbf5242
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-21 11:56:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Font',
-      version='0.2.4',
+      version='0.2.5',
       description='Python module for interacting with Chatbase APIs',
       url='https://github.com/github-noob-arjun/StylishFontBot',
       author='Github',
